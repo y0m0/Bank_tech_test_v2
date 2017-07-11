f7f502370c28e09c16249f6e5698811f7d21c87a
y0m0 <lixi.daniele@gmail.com>
2017-07-11 15:24:50 +0100

    TransactionHistory can create new transactions - store transactions - retrieve transactions

--- spec/transactionHistorySpec.js
+++ spec/transactionHistorySpec.js
@@ -6,11 +6,21 @@ describe('TransactionHistory', () => {
 
   beforeEach(() => {
     transaction = jasmine.createSpy('transaction');
-    transactionHistory = new TransactionHistory(transaction);
+    transactionHistory = new TransactionHistory();
   });
 
   describe('#addTransaction', () => {
-    transactionHistory.addTransaction();
-    expect(transactionHistory.getTransactions).toEqual([transaction]);
+    it('adds a new transaction to transactions', () => {
+      var amount = 10.0;
+      transactionHistory.addTransaction(amount);
+      expect(transactionHistory.transactions.length).toEqual(1);
+    });
+  });
+
+  describe('#getTransactions', () => {
+    it('returns all the transactions', () => {
+      transactionHistory.transactions.push(transaction);
+      expect(transactionHistory.getTransactions()).toEqual([transaction]);
+    });
   });
 });
